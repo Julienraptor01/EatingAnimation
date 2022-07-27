87505a75b3a18656f2df6d644b4f03afbc0b35ac
Julienraptor01 <julienraptor01@gmail.com>
2022-07-27 12:35:06 +0200

    Revert Item GUI Rendering fix

--- src/main/java/ru/tpsd/eatinganimationmod/mixin/ItemRendererMixin.java
+++ src/main/java/ru/tpsd/eatinganimationmod/mixin/ItemRendererMixin.java
@@ -23,8 +23,6 @@ public abstract class ItemRendererMixin implements SynchronousResourceReloader {
 	@Final
 	private ItemModels models;
 
-	@Shadow public abstract BakedModel getModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed);
-
 	public BakedModel getHeldItemModel_1(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
 
 		BakedModel bakedModel3 = this.models.getModel(stack);
@@ -39,9 +37,6 @@ public abstract class ItemRendererMixin implements SynchronousResourceReloader {
 
 	@ModifyVariable(method = "innerRenderInGui(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("STORE"), ordinal = 0)
 	private BakedModel injected(BakedModel bakedModel,@Nullable LivingEntity entity, ItemStack itemStack,int seed) {
-		if(itemStack.isFood()) {
-			return bakedModel = this.getHeldItemModel_1(itemStack, (World)null, entity, seed);
-		}
-			return this.getModel(itemStack, (World)null, entity, seed);
+		return bakedModel = this.getHeldItemModel_1(itemStack, (World)null, entity, seed);
 	}
 }
